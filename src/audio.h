e0a7f34e5907ac2aa84e6bfebcb2b01d137cb12e
MexrlDev <82407748+MexrlDev@users.noreply.github.com>
2026-09-24 07:34:10 +0300

    Update audio.h

--- src/audio.h
+++ src/audio.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: MIT */
 #ifndef AUDIO_H
 #define AUDIO_H
 #include "core.h"
@@ -6,9 +7,6 @@
 int  audio_init(s32 handle, void *aud_out_fn, void *G);
 void audio_shutdown(void);
 void audio_play(enum asset_id id, float vol);
-int  audio_start_thread(void);
-
-/* Exported so main.c can pump manually if no pthread is available */
 void audio_mix_tick(void);
 
 #endif
